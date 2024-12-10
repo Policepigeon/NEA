77c26dab9178da68d27e2231bc761ddf9efca8c6
Policepigeon <policepigeon01@gmail.com>
2024-12-10 15:16:04 +0000

    removing secrets

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 /*  Google AUTH  */
  
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const GOOGLE_CLIENT_ID = '#clientidhere#';
-const GOOGLE_CLIENT_SECRET = '#clientsecrethere#';
+const GOOGLE_CLIENT_ID = ;
+const GOOGLE_CLIENT_SECRET = ;
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
