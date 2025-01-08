f15eb996967b3885923f80068e7566ed0e15c774
Policepigeon <policepigeon01@gmail.com>
2025-01-08 08:15:25 +0000

    cleaning stuff up

--- Oauth-login/app.js
+++ Oauth-login/app.js
@@ -3,7 +3,7 @@ const client = new Appwrite.Client();
 client
     .setEndpoint('https://cloud.appwrite.io/v1') 
     .setProject('online-ide'); 
-
+    .setKey("standard_fe85db02bc1a32f0c356f167e6e3f19265ed65643fa5842c57c7d985ecb1d92ea64882a4137f22aaf2053397e9a1d7c8ed1dffbd4ea7b8faa97cd9f668188e9d5c88820cb2706dd28f999fa58dab267eb00ac98f1f0431150e20f14c378ccd77b6f04a1f585b409e50e32d264aab170b852fcc2947aa4c8f19ec6fea0ca77323");
 const account = new Appwrite.Account(client);
 
 // Handle Google Login
