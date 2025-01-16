f9d6398e594e62bc0f487d038bfc972e2387f839
Policepigeon <policepigeon01@gmail.com>
2025-01-16 12:55:46 +0000

    the purge of all files

--- appwrite-google-auth/src/appwrite.js
+++
@@ -1,9 +0,0 @@
-// src/appwrite.js
-import { Client, Account, OAuthProvider } from 'appwrite'
-
-const client = new Client()
-client
-  .setEndpoint('https://cloud.appwrite.io/v1')// The Appwrite API endpoint
-  .setProject('online-ide')// Your Appwrite project IDexport const account = new Account(client)
-export { OAuthProvider }
-
